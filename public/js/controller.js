65f842da6aaeea155b5f67187aafa1d75a1d7e8b
turboflanagan <turboflanagan@gmail.com>
2016-02-03 13:43:53 -0500

    add styling, logic for mat selections

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,12 +2,29 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('myController', function($scope, $http){
 
+	$scope.price4 = 85;
+	$scope.price2 = 65;
+
 	$scope.selectMat = function(numMats){
-		$scope.baseMatOption = numMats;  // Instead of numMats, add if statement to say 
+		id="select-mats-img2"
+		if(numMats == 4){
+			ng-value = "true";
+			$scope.baseMatOption = "is 4pc set = $" + $scope.price4;
+		}else if(numMats == 2){
+			$scope.baseMatOption = "is 2pc set = $" + $scope.price2;
+		}else{
+			$scope.baseMatOption = "";
+		};
+
+		// $scope.baseMatOption = numMats;  
+		// Instead of numMats, add if statement to say 
 		// "if numMats = 4... add string of HTML..."
+		//if numMats = 0, clear out the 
 	};
 
+	$scope.addColor = function(matColor){
+		$scope.matColorOption = "selection is " + matColor;
 
-	
+	};
 
-});
+}); 
