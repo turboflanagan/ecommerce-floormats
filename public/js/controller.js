288e66ed0018d083aced21e16a59fa54dba8e5f7
turboflanagan <turboflanagan@gmail.com>
2016-02-09 08:44:42 -0500

    add payment
    Add stripe payment, thank you page and route.
    Fix footer to bottom of pages.

--- public/js/controller.js
+++ public/js/controller.js
@@ -4,13 +4,15 @@ myApp.controller('myController', function($scope, $http){
 
 	$scope.price4 = 85;
 	$scope.price2 = 65;
-	$scope.priceEmbroiderFront = 30;
-	$scope.priceEmbroiderRear = 10;
+	$scope.priceEmbroiderFront = "";
+	$scope.priceEmbroiderRear = "";
 	$scope.priceShip;
 	$scope.matColor = "";
 	$scope.totalCost = 0;
 	var shipping = 12;
 	var is = "is: ";
+	var frontEmbroidery = 30;
+	var rearEmbroidery = 10;
 
 	$scope.selectMat = function(numMats){
 		id="select-mats-img2"
@@ -19,14 +21,14 @@ myApp.controller('myController', function($scope, $http){
 			$scope.matFour = true;
 			$scope.baseMatOption = "is 4pc set = $" + $scope.price4;
 			$scope.priceShip = "$" + shipping;
-			calcTotal($scope.price4, shipping, $scope.priceEmbroiderFront, null);
+			calcTotal($scope.price4, shipping, frontEmbroidery, null);
 			$(this).css("border", "3px solid yellow");
 		}else if(numMats == 2){
 			$scope.matFour = false;
 			$scope.matTwo = true;
 			$scope.baseMatOption = "is 2pc set = $" + $scope.price2;
 			$scope.priceShip = "$" + shipping;
-			calcTotal($scope.price2, shipping, $scope.priceEmbroiderFront, null);
+			calcTotal($scope.price2, shipping, frontEmbroidery, null);
 			
 		}else{
 			$scope.baseMatOption = "";
@@ -39,38 +41,15 @@ myApp.controller('myController', function($scope, $http){
 		//Get matCost
 		$scope.matCost = 0;
 		if($scope.matTwo){
-			$scope.matCost = $scope.price2 + shipping + priceEmbroiderFront;
+			$scope.matCost = $scope.price2 + shipping + frontEmbroidery;
 			$scope.totalCost = $scope.matCost;
-
-
-
-
-
-
-
-
-
-
-
-			console.log("*********totalCost************");
-			console.log($scope.totalCost);
-
-			console.log("********** shipping *************");
-			console.log(shipping);
-
-			console.log("********* priceEmbroiderFront **********");
-			console.log(priceEmbroiderFront);
-
-			console.log("*********** matCost *************");
-			console.log(matCost);
-
+		}else if($scope.matFour){
+			$scope.matCost = $scope.price4 + shipping + frontEmbroidery + rearEmbroidery;
+			$scope.totalCost = $scope.matCost;
 		}
 	};
 
-	// $scope.addColor = function(setMatColor){
-	// 	$scope.matColorOption = "selection is " + matColor;
 
-	// };
 
 
 	$scope.setColor = function(matColor){
@@ -98,14 +77,17 @@ myApp.controller('myController', function($scope, $http){
 
 	$scope.setStyle = function(style){
 		$scope.optionStyle = is + style;
+		$scope.priceEmbroiderFront = frontEmbroidery;
 		$scope.styleCost = " $" + $scope.priceEmbroiderFront;
 	};
 
 	$scope.addCarNumber = function(){
 		if($scope.carNumberNone){
 			$scope.addedCarNumber = 'None';
+			$scope.priceEmbroiderRear = 0;
 		}else{
 			$scope.addedCarNumber = $scope.carNumber;
+			$scope.priceEmbroiderRear = rearEmbroidery;
 		}
 		event.preventDefault();
 	}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/************************ GET home page route ************************/
+/* . */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/************************ PAYMENT PAGE ROUTES ************************/
+router.get('/payment', function (req, res, next) {
+// if(req.session.username){
+    res.render('payment');
+    // }
+});
+
+router.post('/payment', function (req, res, next) {
+            res.redirect('thank-you')
+    
+    // if(req.session.username){
+    //     stripe.charges.create({
+    //       amount: 400,
+    //       currency: "usd",
+    //       source: req.body.stripeToken, // obtained with Stripe.js
+    //       description: "Charge for " + req.body.stripeEmail
+    //     }, function(err, charge) {
+    //       // asynchronously called
+    //       if(err){
+    //         res.send('You got an err.' + err)
+    //       }else{
+    //         res.redirect('thank-you')
+    //       }
+    //     });
+    // }
+});
+
+router.get('/thank-you', function (req, res, next){
+        res.render('thank-you')
+});
+
+
+
+
+
 module.exports = router;
+
+
+
+
+    // res.render('payment', { username: req.session.username, menuItem: 'payment' });
